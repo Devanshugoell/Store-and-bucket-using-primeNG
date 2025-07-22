860a99ca5a76f487bd1e66c1992c08a27c7497f0
Devanshu <devgoyal952@gmail.com>
2025-07-22 10:33:44 +0530

    Developed header using primeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,59 @@
 import { Component } from "@angular/core";
+import { MenuItem } from "primeng/api";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+import { MenubarModule } from "primeng/menubar";
+import { DialogModule } from "primeng/dialog";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MenubarModule,
+    DialogModule,
+    InputTextModule,
+    ButtonModule,
+  ],
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = "storeAndBucket2";
+  items: MenuItem[] = [];
+
+  showStoreForm = false;
+  storeName = "";
+  storeLocation = "";
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: "Store",
+        icon: "pi pi-store",
+        command: () => {
+          this.showStoreForm = true;
+        },
+      },
+      {
+        label: "Bucket",
+        icon: "pi pi-shopping-cart",
+        command: () => {
+          alert("Bucket clicked!");
+        },
+      },
+    ];
+  }
+
+  submitForm() {
+    console.log("Store Name:", this.storeName);
+    console.log("Store Location:", this.storeLocation);
+    this.showStoreForm = false;
+
+    // Clear form
+    this.storeName = "";
+    this.storeLocation = "";
+  }
 }
