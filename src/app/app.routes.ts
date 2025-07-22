5132ee779a4999c86148e311e279860b2a0cf483
Devanshu <devgoyal952@gmail.com>
2025-07-22 15:52:01 +0530

    Developed home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from "@angular/router";
 import { StoreComponent } from "./store/store.component";
 import { BucketComponent } from "./bucket/bucket.component";
 import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
 
 export const routes: Routes = [
   { path: "store", component: StoreComponent },
   { path: "bucket", component: BucketComponent },
-  { path: "", component: AppComponent },
+  { path: "", component: HomeComponent },
 ];
