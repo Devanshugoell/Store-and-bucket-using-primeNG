683defe441dc6b4d349027b59b8afcbe7a5df72f
Devanshu <devgoyal952@gmail.com>
2025-07-22 15:50:18 +0530

    developed store and bucket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,25 @@
 import { Component } from "@angular/core";
-import { MenuItem } from "primeng/api";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-
+import { RouterModule } from "@angular/router";
 import { MenubarModule } from "primeng/menubar";
-import { DialogModule } from "primeng/dialog";
-import { InputTextModule } from "primeng/inputtext";
-import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    MenubarModule,
-    DialogModule,
-    InputTextModule,
-    ButtonModule,
-  ],
+  imports: [RouterModule, MenubarModule],
   templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  items: MenuItem[] = [];
-
-  showStoreForm = false;
-  storeName = "";
-  storeLocation = "";
-
-  ngOnInit() {
-    this.items = [
-      {
-        label: "Store",
-        icon: "pi pi-store",
-        command: () => {
-          this.showStoreForm = true;
-        },
-      },
-      {
-        label: "Bucket",
-        icon: "pi pi-shopping-cart",
-        command: () => {
-          alert("Bucket clicked!");
-        },
-      },
-    ];
-  }
-
-  submitForm() {
-    console.log("Store Name:", this.storeName);
-    console.log("Store Location:", this.storeLocation);
-    this.showStoreForm = false;
-
-    // Clear form
-    this.storeName = "";
-    this.storeLocation = "";
-  }
+  items = [
+    {
+      label: "Store",
+      icon: "pi pi-home",
+      routerLink: "/store",
+    },
+    {
+      label: "Bucket",
+      icon: "pi pi-shopping-cart",
+      routerLink: "/bucket",
+    },
+  ];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { StoreComponent } from "./store/store.component";
+import { BucketComponent } from "./bucket/bucket.component";
+import { AppComponent } from "./app.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "store", component: StoreComponent },
+  { path: "bucket", component: BucketComponent },
+  { path: "", component: AppComponent },
+];
